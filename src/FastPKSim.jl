de1be3f66251a52c75f00c5276c0575b0e37de61
Ylva Wahlquist <ylva.wahlquist@control.lth.se>
2022-09-29 10:52:52 +0200

    add example code and example test

--- src/FastPKSim.jl
+++ src/FastPKSim.jl
@@ -1,5 +1,19 @@
 module FastPKSim
 
+export main
+
 # Write your package code here.
+function main()
+    n = 10
+    a = zeros(n)
+    for i = 1:n
+        a[i] = squareroot(i)
+    end
+    return a
+end
+
+function squareroot(x)
+    return sqrt(x)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "FastPKSim.jl" begin
     # Write your tests here.
+    @test [2,1] + [1,2] == [3,3]
+
+    @test main() == sqrt.(1:10)
+
 end
