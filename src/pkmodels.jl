9662948807c2cd7512c059e3df2aa03b176a1969
Ylva Wahlquist <ylva.wahlquist@control.lth.se>
2022-10-20 16:33:05 +0200

    split compartment models to separate functions

--- src/pkmodels.jl
+++ src/pkmodels.jl
@@ -3,85 +3,89 @@
 
 using StaticArrays, LinearAlgebra
 
-
-struct PK{T,V,L,LI,S,O}
-    θ::T
-    V1inv::V
-    λ::L
-    λinv::LI
-    R::S
-    order::O
+# Initiate/update parameters
+function PK1(θ)
+    V1inv = 1 / θ[6]
+    λ = -θ[1]
+    λinv = 1 / λ
+    R = one(1)
+    return V1inv, λ, λinv, R
 end
 
-function PK(θ, order) # constructor
-    λ = getλ(θ, order)
-    R = getR(θ, λ, order)
-    V1inv = 1 / θ[end]
+# Initiate/update parameters
+function PK2(θ)
+    V1inv = 1 / θ[6]
+    λ = getλ_twocomp(θ)
     λinv = 1 ./ λ
-    PK(θ, V1inv, λ, λinv, R, order)
+    R = getR_twocomp(θ, λ)
+    return V1inv, λ, λinv, R
+end
+
+# Initiate/update parameters
+function PK3(θ)
+    V1inv = 1 / θ[6]
+    λ = getλ_threecomp(θ)
+    λinv = 1 ./ λ
+    R = getR_threecomp(θ, λ)
+    return V1inv, λ, λinv, R
 end
 
 
-@inline @fastmath function getλ(θ::AbstractVector{T}, order) where {T}
-    if order == 1
-        return -θ[1]
+@inline @fastmath function getλ_twocomp(θ::AbstractVector{T}) where {T}
+    k10, k12, k21, _ = θ
+    a1 = (k10 + k12 + k21) / 2
+    a2 = sqrt(a1^2 - k10 * k21)
+    return @SVector [-a1 - a2, -a1 + a2] # The eigenvalues of the continuous-time system matrix
+end
 
-    elseif order == 2
-        k10, k12, k21, _ = θ
-        a1 = (k10 + k12 + k21) / 2
-        a2 = sqrt(a1^2 - k10 * k21)
-        return @SVector [-a1 - a2, -a1 + a2] # The eigenvalues of the continuous-time system matrix
+# # Computation of R, nominators of the first-order systems
+@inline function getR_twocomp(θ, λ) # samma för R
+    _, _, k21, _ = θ
+    l1, l2 = λ
+    a1 = l1 - l2
+    a1inv = 1 / a1
+    Qinv = @SMatrix [l1*a1inv -a1inv; -l2*a1inv a1inv]
+    b = @SVector [1, k21] # First column of P, only interested in the first output, x1
+    return Qinv * b
+end
 
-    elseif order == 3
+# Compute eigenvalues λ for 3 compartment mammillary model
+@inline @fastmath function getλ_threecomp(θ::AbstractVector{T}) where {T}
+    k10, k12, k13, k21, k31, _ = θ
+    b1 = k10 + k12 + k13 + k21 + k31
+    b2 = k21 * (k10 + k13 + k31) + k31 * (k10 + k12)
+    b3 = k10 * k21 * k31
 
-        k10, k12, k13, k21, k31, _ = θ
-        b1 = k10 + k12 + k13 + k21 + k31
-        b2 = k21 * (k10 + k13 + k31) + k31 * (k10 + k12)
-        b3 = k10 * k21 * k31
+    # Wengert list used to compute λ.
+    a1 = b1 / 3
+    a2 = a1^2
+    a3 = a1 * a2
+    a4 = b2 / 3
+    a5 = a4 - a2
+    a6 = (b1 * a4 - b3) / 2
+    a7 = 2(a6 + sqrt(complex(a5^3 + (a3 - a6)^2)) - a3)^(1 / T(3))
+    a8 = -real(a7)
+    a9 = imag(a7)
+    a10 = a9 * sqrt(T(3)) / 2
+    a11 = a1 - a8 / 2
 
-        # Wengert list used to compute λ.
-        a1 = b1 / 3
-        a2 = a1^2
-        a3 = a1 * a2
-        a4 = b2 / 3
-        a5 = a4 - a2
-        a6 = (b1 * a4 - b3) / 2
-        a7 = 2(a6 + sqrt(complex(a5^3 + (a3 - a6)^2)) - a3)^(1 / T(3))
-        a8 = -real(a7)
-        a9 = imag(a7)
-        a10 = a9 * sqrt(T(3)) / 2
-        a11 = a1 - a8 / 2
-
-        return @SVector [-a1 - a8, -a10 - a11, a10 - a11] # The eigenvalues of the continuous-time system matrix
-    end
+    return @SVector [-a1 - a8, -a10 - a11, a10 - a11] # The eigenvalues of the continuous-time system matrix
 end
 
 # Computation of R, nominators of the first-order systems
-@inline function getR(θ, λ, order) # samma för R
-    if order == 1
-        return one(1)
-    elseif order == 2
-        _, _, k21, _ = θ
-        l1, l2 = λ
-        a1 = l1 - l2
-        a1inv = 1 / a1
-        Qinv = @SMatrix [l1*a1inv -a1inv; -l2*a1inv a1inv]
-        b = @SVector [1, k21] # First column of P, only interested in the first output, x1
-        return Qinv * b
-    elseif order == 3
-        _, _, _, k21, k31, _ = θ
-        l1, l2, l3 = λ
-        a1 = l2 - l1
-        a2 = l3 - l1
-        a3 = l3 - l2
-        d1 = a2 * a1
-        d2 = -a3 * a1
-        d3 = a2 * a3
-        d1inv = 1 / d1
-        d2inv = 1 / d2
-        d3inv = 1 / d3
-        Qinv = @SMatrix [(l1*d1inv)*l1 l1*d1inv d1inv; (l2*d2inv)*l2 l2*d2inv d2inv; (l3*d3inv)*l3 l3*d3inv d3inv]
-        b = @SVector [1, k21 + k31, k21 * k31] # Quite often we would only be interested in the first column (first output). See paper for computations.
-        return Qinv * b
-    end
+@inline function getR_threecomp(θ, λ)
+    _, _, _, k21, k31, _ = θ
+    l1, l2, l3 = λ
+    a1 = l2 - l1
+    a2 = l3 - l1
+    a3 = l3 - l2
+    d1 = a2 * a1
+    d2 = -a3 * a1
+    d3 = a2 * a3
+    d1inv = 1 / d1
+    d2inv = 1 / d2
+    d3inv = 1 / d3
+    Qinv = @SMatrix [(l1*d1inv)*l1 l1*d1inv d1inv; (l2*d2inv)*l2 l2*d2inv d2inv; (l3*d3inv)*l3 l3*d3inv d3inv]
+    b = @SVector [1, k21 + k31, k21 * k31] # Quite often we would only be interested in the first column (first output). See paper for computations.
+    return Qinv * b
 end
--- src/simulation.jl
+++ src/simulation.jl
@@ -45,7 +45,7 @@ end
 end
 
 """
-    pksim!(y, θ, u, v, hs, youts)
+    pk3sim!(y, θ, u, v, hs, youts)
 Fast simulation of the three compartment mammillary PK model.
 
 The parameter vector θ has the following structure
@@ -59,21 +59,20 @@ The parameter vector θ has the following structure
 - `v`: Bolus dose vector of size length(hs)
 - `hs`: Step size, should have the size of [diff(time) diff(time)[end]] where time is the matching time vector to u, v
 - `youts`: Indices for output observations, corresponding to times in hs
-- `order`: Model order. If argument is left out, order = 3
 
 Updates `y` with simulated outputs `x_1` at time instances `youts`.
 """
-function pksim!(y, θ, u, v, hs, youts; order = 3)
-    model = PK(θ, order)
+function pk3sim!(y, θ, u, v, hs, youts)
+    V1inv, λ, λinv, R = PK3(θ)
     j = 1 # counter to keep track of next free spot in y
-    x = @SVector zeros(eltype(u), order) # initial state
+    x = @SVector zeros(eltype(u), 3) # initial state
     for i in eachindex(u, hs, v)
         if i in youts # if we want to compute output
-            x, yi = @inbounds updatestateoutput(x, hs[i], model.V1inv, model.λ, model.λinv, model.R, u[i], v[i]) # update state and compute output
+            x, yi = @inbounds updatestateoutput(x, hs[i], V1inv, λ, λinv, R, u[i], v[i]) # update state and compute output
             y[j] = yi
             j += 1
         else
-            x = @inbounds updatestate(x, hs[i], model.λ, model.λinv, u[i], v[i]) # update state
+            x = @inbounds updatestate(x, hs[i], λ, λinv, u[i], v[i]) # update state
         end
     end
     return y
